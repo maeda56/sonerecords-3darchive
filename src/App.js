3a10fd82afbe4f72136011defb534cfe87a43ef9
makinori <maemaeakiaki123@gmail.com>
2023-10-15 19:37:56 +0900

    run model-viewer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+// import Home from './Home';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <Home />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import "./App.css";
+import { Helmet } from "react-helmet";
+import SoneRecoreds from "./Sonerecords20231014.glb"
+
+export default function App() {
+  React.useEffect(() => {
+    document
+      .getElementById("my-model-viewer")
+      .addEventListener("error", e =>
+        console.log(`Error: ${JSON.stringify(e)}`)
+      );
+    document
+      .getElementById("my-model-viewer")
+      .addEventListener("load", e => console.log(`Load: ${JSON.stringify(e)}`));
+    document
+      .getElementById("my-model-viewer")
+      .addEventListener("ar-status", e =>
+        console.log(`AR-Status: ${JSON.stringify(e)}`)
+      );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <script
+          type="module"
+          src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"
+        />
+        <script
+          nomodule
+          src="https://unpkg.com/@google/model-viewer/dist/model-viewer-legacy.js"
+        />
+      </Helmet>
+      <div class="home__title">
+            <div class="home__sentence">
+                Sone Records ゆりのき通り店舗 <br></br>3D Archive
+            </div>
+        </div>
+      <div className="model-viewer">
+        <model-viewer
+          id="my-model-viewer"
+          alt="astronaut"
+          src={SoneRecoreds}
+          auto-rotate
+          camera-controls
+        />
+      </div>
     </div>
   );
 }
-
-export default App;
